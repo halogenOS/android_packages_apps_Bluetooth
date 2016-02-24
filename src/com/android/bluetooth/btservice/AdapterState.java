32d2e0e197287723611b938978ec36a87b3ec030
Ajay Kumar <ajayku@codeaurora.org>
2016-02-24 14:26:15 +0530

    Bluetooth: Taking corrective actios in case  of timeout
    Enable_timeout: stopping gatt profiles also along with
    other profiles.
    
    At BLE_Stop_timeout: app will move to state OFF while
    we are not sure whether stack also stopped BLE SM.So
    restarting the app.
    
    At start timeout : if BREDR profiles are not stopped at
    start timeout, we will move on to BLE_ON and from
    there to OFF state and stack will be shut down while
    we did not stop the profiles that are already started.
    
    CRs-Fixed: 956077
    Change-Id: I1a584c3ea8b60949f582f62e720e6bfb0bf19f44

--- src/com/android/bluetooth/btservice/AdapterState.java
+++ src/com/android/bluetooth/btservice/AdapterState.java
@@ -436,8 +436,11 @@ final class AdapterState extends StateMachine {
                 case BREDR_START_TIMEOUT:
                     errorLog("Error enabling Bluetooth (start timeout)");
                     mPendingCommandState.setTurningOn(false);
+                    adapterService.stopProfileServices();
                     transitionTo(mBleOnState);
                     notifyAdapterStateChange(BluetoothAdapter.STATE_BLE_ON);
+                    errorLog("ENABLE_TIMEOUT:Killing the process to force a restart as part cleanup");
+                    android.os.Process.killProcess(android.os.Process.myPid());
                     break;
 
                 case ENABLE_TIMEOUT:
@@ -447,13 +450,15 @@ final class AdapterState extends StateMachine {
                     transitionTo(mOffState);
                     adapterService.stopProfileServices();
                     notifyAdapterStateChange(BluetoothAdapter.STATE_OFF);
+                    errorLog("ENABLE_TIMEOUT:Killing the process to force a restart as part cleanup");
+                    android.os.Process.killProcess(android.os.Process.myPid());
                     break;
 
                 case BREDR_STOP_TIMEOUT:
                     errorLog("Error stopping Bluetooth profiles (stop timeout)");
                     mPendingCommandState.setTurningOff(false);
                     transitionTo(mBleOnState);
-                    notifyAdapterStateChange(BluetoothAdapter.STATE_BLE_ON);
+                    notifyAdapterStateChange(BluetoothAdapter.STATE_OFF);
                     errorLog("BREDR_STOP_TIMEOUT:Killing the process to force a restart as part cleanup");
                     android.os.Process.killProcess(android.os.Process.myPid());
                     break;
@@ -463,6 +468,8 @@ final class AdapterState extends StateMachine {
                     mPendingCommandState.setTurningOff(false);
                     transitionTo(mOffState);
                     notifyAdapterStateChange(BluetoothAdapter.STATE_OFF);
+                    errorLog("BLE_STOP_TIMEOUT:Killing the process to force a restart as part cleanup");
+                    android.os.Process.killProcess(android.os.Process.myPid());
                     break;
 
                 case DISABLE_TIMEOUT:
